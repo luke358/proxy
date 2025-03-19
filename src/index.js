18f851f895cb05c248a0207806dc185f6a066501
luke358 <qq1494135711@gmail.com>
2025-03-19 17:18:21 +0800

    feat: 优化

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import cors from 'cors';
 import { checkWhitelist, whitelist } from './middleware/index.js';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 const { json } = bodyParser;
+import http from 'http';
 
-const proxyPool = {
-  'binanceFutures': 'https://fapi.binance.com/fapi/v1/klines',
-  'binance': 'https://api.binance.com/api/v3/klines',
-  'okx': 'https://www.okx.com/api/v5/market/candles',
-  'data': 'http://data.kernel-trading.com/ohlcv/data',
-};
+const agent = new http.Agent({
+  keepAlive: true, // 复用连接
+  maxSockets: 100, // 最大并发连接数
+});
+
+const proxyMiddlewareMap = {}
 async function bootstrap() {
   const app = express();
   app.use(cors())
@@ -31,18 +32,19 @@ async function bootstrap() {
 
     url = decodeURIComponent(url);
     console.log("代理到 url", url);
-    const proxyMiddleware = createProxyMiddleware({
+    const proxyMiddleware = proxyMiddlewareMap[url] || (proxyMiddlewareMap[url] = createProxyMiddleware({
       target: url, // 目标服务器地址
       changeOrigin: true, // 改变源请求的 origin
-      logLevel: 'debug', // 日志级别
+      logLevel: 'error', // 日志级别
       timeout: 5000, // 设置代理超时时间（5秒）
+      agent,
       pathRewrite: (resPath, req) => {
         let path = resPath.replaceAll('/', '');
         const params = new URLSearchParams(req.query);
         params.delete('url');
         return path.split('?')[0] + '?' + params.toString();
       },
-    });
+    }));
     proxyMiddleware(req, res, next);
   });
 
@@ -57,19 +59,6 @@ async function bootstrap() {
   const port = 6602;
   app.listen(port, async () => {
     console.log(`代理服务器已启动，监听端口 ${port}`);
-    // TODO: register to proxy pool
-    for (const ip of whitelist.filter(item => item !== '127.0.0.1')) {
-      try {
-        const response = await fetch(`http://${ip}:6602/ping`);
-        if (response.ok) {
-          console.log(`已成功注册到代理池:${ip}`);
-        } else {
-          console.error(`注册到代理池失败：${ip}`);
-        }
-      } catch (error) {
-        console.error(`注册到代理池失败：${ip}`);
-      }
-    }
   });
 }
 
